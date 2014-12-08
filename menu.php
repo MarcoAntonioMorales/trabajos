99b1090b7bd736d6d138dddc79364889610c486e
MarcoAntonioMorales <marcoantonio_moralesmtz@hotmail.com>
2014-12-08 00:16:53 -0600

    primer sistema

---
+++ menu.php
@@ -0,0 +1,23 @@
+<div class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><b>Sistema Escolar</b></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php"><b>Inicio</b></a></li>
+                <li class="active"><a href="testUsuario.php"><b>Administraci&oacute;n <br> Usuario</b></a></li>
+                <li><a href="testMateria.php"><b>Administraci&oacute;n <br> Materia</b></a></li>
+                <li><a href="testGrupo.php"><b>Administraci&oacute;n <br> Grupos</b></a></li>
+                <li><a href="TestMateria1.php"><b>Agregar Materias <br>A Maestros</b></a></li>
+                <li><a href="agregaalumno.php"><b>Agregar Alumnos <br>A Grupos</b></a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
